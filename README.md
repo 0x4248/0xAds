f95743ae39acaf97d88cca6363639ad35479c607
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-12-30 22:31:07 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 0xAds
+
+An open-source, no backend advertisement system for websites.
+
+*Work in progress*
+
+## Licence
+
+This project is licenced under the GNU General Public Licence v3.0.
---
+++ xAds-Index.js
@@ -0,0 +1,27 @@
+
+function fetchAds() {
+  var xAdsContainer = document.getElementById('xAds-Container');
+  if (!xAdsContainer) {
+	console.error('xAds-Container not found');
+	return;
+  }
+
+  fetch('/list.json')
+	.then(function(response) {
+	  return response.json();
+	})
+	.then(function(json) {
+	  var randomAd = json[Math.floor(Math.random() * json.length)];
+	  fetch('/ads/' + randomAd + '.json')
+		.then(function(response) {
+		  return response.json();
+		})
+		.then(function(json) {
+		  var ad = document.createElement('div');
+		  ad.innerHTML = '<p class="xAds-Title">' + json.title + '</p>' + '<p class="xAds-Description">' + json.description + '</p>' + '<button class="xAds-Button"><a class="xAds-Link" href="' + json.link + '">' + json.prompt + '</a></button>';
+		  xAdsContainer.appendChild(ad);
+		});
+	});
+}
+
+fetchAds();
